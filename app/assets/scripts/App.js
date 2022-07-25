6dca76d50f94d321b11e6df13a5c87410e58afb9
Vic <victsang001@gmail.com>
2022-07-25 08:06:03 +0800

    Completed our build process

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -5,7 +5,6 @@ import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
 
 
-
 new StickyHeader()
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
